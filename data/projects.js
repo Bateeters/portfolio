cd47fa51369252efd8bd43d85e637f16031f5f45
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-01-09 10:58:58 -0500

    reworked dashboard layout. swapped css grid for bootstrap cols

---
+++ data/projects.js
@@ -0,0 +1,35 @@
+export const projects = [
+    {
+        id: "1",
+        title: "Project 1",
+        description: "...",
+        tech: ["HTML", "CSS", "JS"],
+        image: "/assets/images/projects/1.png",
+        links: {
+            live: "...",
+            repo: "..."
+        }
+    },
+    {
+        id: "2",
+        title: "Project 2",
+        description: "...",
+        tech: ["HTML", "CSS", "JS"],
+        image: "/assets/images/projects/1.png",
+        links: {
+            live: "...",
+            repo: "..."
+        }
+    },
+    {
+        id: "3",
+        title: "Project 3",
+        description: "...",
+        tech: ["HTML", "CSS", "JS"],
+        image: "/assets/images/projects/1.png",
+        links: {
+            live: "...",
+            repo: "..."
+        }
+    },
+];
